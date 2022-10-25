bd20961bc0338a19c71dbc5d55f91654e7af6957
thiago.pessoa <thiagopessoa246@gmail.com>
2022-10-25 17:08:17 -0300

    att

---
+++ atividadeSalarioBruto.py
@@ -0,0 +1,10 @@
+print("Digite seu salario base: ")
+
+salarioBase = float(input())
+gratificacao = float(input("Digite a gratificacao: "))
+
+#Processamento
+salarioBruto = salarioBase + gratificacao
+
+#saída
+print ("Seu salário bruto é R$", salarioBruto)
