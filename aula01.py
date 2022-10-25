ff00e45befecb97ea8a7efaa45d0e6c5673c6f17
thiago.pessoa <thiagopessoa246@gmail.com>
2022-10-25 16:57:23 -0300

    att

---
+++ aula01.py
@@ -0,0 +1,13 @@
+"""existem tres modos de adicionar comentarios ao python
+Shell, direto no python IDLE (Ambiente de desenvolvimento integrado) (Interavel), Modo script (Escrever um arquivo .py)""""
+
+nome = "Thiago"
+idade = 18
+altura = 1.73
+branco = True
+
+print(nome,type(nome))
+#metodo type diz o tipo primitivo da variavel
+print (idade, type(idade))
+print (altura, type (altura))
+print (branco, type (branco))
