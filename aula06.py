2fd0f372c2a9180c08309fa10fde28ae50a0efd6
Thiago Pessoa <104585635+thidspessoa@users.noreply.github.com>
2022-11-11 17:01:10 -0300

    Add files via upload

---
+++ aula06.py
@@ -0,0 +1,180 @@
+"""
+print("Bem vindo ao jogo! Ao final, você terá uma pontuação baseada nos seus acertos. Vamos a primeira pergunta:")
+
+print("\nEm que ano foi lançado o primeiro filme de Star Wars no cinema? ")
+resposta1 = input("Sua resposta: ")
+
+print("\nQual o nome do mestre Jedi que é verde e pequeno?")
+resposta2 = input("Sua resposta: ")
+
+print("Qual o primeiro nome do protagonista da trilogia original de Star Wars?")
+resposta3 = input("Sua resposta: ")
+
+print("O nome do vilão que se veste de preto e tem uma voz metálica é Darth ...")
+resposta4 = input("Sua resposta: ")
+
+print( "Escreva a letra da opção que NÃO é o nome de um título de filme da saga Star Wars. \n A) Uma nova Esperança \n B) O Retorno do Jedi \n C) Os Jedis Contra Atacam ")
+resposta5 = input("Sua resposta: ")
+
+if (resposta1 == "1997"):
+    print("Parabéns! Você é um(a) Nerd Raiz.")
+"""    
+
+#Atividade sobre orientação a objetos:
+"""
+É possivel utilizar classes sem construtores, mas os construtores são 
+usados para campos oobrigatorios de serem preenchidos no seu site. No banco de dados
+seria o comando not null do MYSQQL, so permitindo o envio das informações caso
+seja colocado algum dado como parametro para os contrutores.
+"""
+#as classes devem iniciar com letra maiuscula em python.
+#metodo self serve para dizer que as informações e variaveis dentro da classe pertencem a ela;
+class Pessoa(): #mãe 
+    def __init__(self, nome, idade):
+        self.__nome = nome #"__" é um mutator, oculta a instância.
+        self.__idade = idade
+        
+    def get_nome(self):
+        return self.__nome
+    
+    def get_idade(self):
+        return self.__idade
+    
+    def set_idade(self, nova_idade):
+        self.__idade = nova_idade
+    
+    def mensagem(self):
+        print("\nSeja bem vindo!")
+        
+class PessoaFisica(Pessoa): #filha esta erdando a função mensagem da classe PESSOA.
+    def __init__(self, nome, idade, CPF):
+        super().__init__(nome, idade)
+        self.CPF = CPF
+        
+    def calculoImposto(self, *salario): #função #transformar a o parametro salario em uma tupla, faz com que o usuario possa inserir mais de uma vez um valor nesta variavel.
+        
+        #for faz varredura em uma tupla;
+        salarios = 0 #acumulador
+        for e in salario:
+            ir = e * 0.015
+            print("Seu imposto de renda é R$: ", ir)
+            salarios += ir
+        return salarios
+
+class PessoaJuridica(): #filha
+    def __init__(self, nome, idade, CNPJ):
+        self.__nome = nome
+        self.__idade = idade
+        self.CNPJ = CNPJ
+
+    def get_nome(self):
+        return self.__nome
+    
+    def get_idade(self):
+        return self.__idade
+        
+    def mensagem(self):
+        print("\nSeja bem vindo!")
+    
+    def calculoImposto(self, salario, porteEmpresa): #função
+        if (porteEmpresa == "pequeno"):
+            print(salario*0.10)
+        elif(porteEmpresa == "medio" or porteEmpresa == "médio"):
+            print(salario*0.15)
+        elif(porteEmpresa == "grande"):
+            print(salario*0.20) 
+         
+        #print(salario * 0.10)
+
+
+n = str(input("Digite o nome: "))
+c = str(input("Digite a idade: "))
+j = str(input("Digite o tipo de pessoa: "))
+
+#Input com estrutura de repetição
+s = [] #iniciamos como lista pois tuplas não podem ser mudadaas de tamanho, e apos isso convertemos em tupla.
+while True:
+    sal = float(input("salário: "))
+    s.append(sal)
+    encerrar = int(input("Ainda tem salário? 1-sim, 2-não"))
+    if (encerrar == 1):
+        continue
+    else:
+        tupla = tuple(s)#transforma em tupla
+        print(tupla) 
+        break
+    
+
+
+#INSTANCIAMENTO
+
+pessoa1 = Pessoa(n, c) #variavel de referencia para chamar a classe.
+#Instanciamos a classe dentro da variavel passando parametros.
+
+
+if (j == "Fisica" or j == "fisica"):
+    x = str(input("Digite o CPF: "))         
+    pessoa2 = PessoaFisica(n, c, x)
+    print(pessoa2.mensagem())
+    print(pessoa2.get_nome())
+    print(pessoa2.get_idade())
+    print(pessoa2.CPF)
+    print(pessoa2.calculoImposto(tupla))
+    
+elif (j == "Juridica" or j == "juridica"):
+    y = str(input("Digite o CNPJ: "))
+    i = str(input("Digite o porte da empresa: "))
+    pessoa3 = PessoaJuridica(n, c, y)
+    print(pessoa3.mensagem())
+    print(pessoa3.get_nome())
+    print(pessoa3.get_idade())
+    print(pessoa3.CNPJ)
+    print(pessoa3.calculoImposto(tupla, i))
+
+
+
+"""
+print(pessoa1.get_nome()) #encapsulamento
+print(pessoa1.set_idade(22)) #encapsulamento
+print(pessoa1.get_idade())
+"""
+
+
+"""
+pessoa1 = PessoaJuridica(n, c, y)
+print(pessoa1.mensagem())
+print(pessoa1.nome)
+print(pessoa1.idade)
+print(pessoa1.CNPJ)
+print(pessoa1.calculoImposto(123993, i))
+"""
+
+"""
+pessoa3 = PessoaFisica(n, c, x)
+print(pessoa3.mensagem())
+print(pessoa3.nome)
+print(pessoa3.idade)
+print(pessoa3.CPF)
+print(pessoa3.calculoImposto(233, 4444, 1212))
+"""    
+
+
+"""
+pessoa2 = PessoaFisica("Jose", 18, 897498734)
+print(pessoa2.mensagem())
+print(pessoa2.CPF)
+print(pessoa2.idade)
+print(pessoa2.nome)
+"""    
+    
+    
+    
+    
+    
+
+#METODOS
+"""
+dir + o comando: mostra detalhes soobre o ocmando, metodos e etc.
+Ex:
+print(dir(lista))
+"""
